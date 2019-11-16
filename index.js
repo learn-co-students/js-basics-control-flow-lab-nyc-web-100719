374253b35f7f415a0fd9b5a295be48af6f13ea63
LainaJ <lainakarosic@gmail.com>
2019-11-16 18:00:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideFeet){
+  // let result; 
+    if (rideFeet <= 400) {
+      return 'This one is on me!'; 
+    } else if (rideFeet > 2000 && rideFeet < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
+  }
+
+function ternaryCheckCity(city){
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ 
+let response;
+ 
+switch (tip) {
+    case 'generous':
+        response = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        response = 'Thank you.';
+        break;
+    case 'thanks for everything':
+        response = 'Bye.';
+        break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+ 
+return response;
 }
